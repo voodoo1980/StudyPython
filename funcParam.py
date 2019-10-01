42d23a590abc9e7b21a051cf52c8e377d182578c
eric <flyingcat.008@gmail.com>
2019-10-01 15:16:39 +0800

    update the codes

--- funcParam.py
+++ funcParam.py
@@ -1,3 +1,31 @@
 # coding=utf-8
 
-def
+def product(*param):
+    if len(param) <= 0:
+        raise TypeError("Empty input")
+
+    else:
+        result = 1
+        for n in param:
+            result *= n
+    return result
+
+
+print('product(5) =', product(5))
+print('product(5, 6) =', product(5, 6))
+print('product(5, 6, 7) =', product(5, 6, 7))
+print('product(5, 6, 7, 9) =', product(5, 6, 7, 9))
+if product(5) != 5:
+    print('测试失败!')
+elif product(5, 6) != 30:
+    print('测试失败!')
+elif product(5, 6, 7) != 210:
+    print('测试失败!')
+elif product(5, 6, 7, 9) != 1890:
+    print('测试失败!')
+else:
+    try:
+        product()
+        print('测试失败!')
+    except TypeError:
+        print('测试成功!')
