188d78d321da3f270aaee7a3a6aa703983a10624
eric <flyingcat.008@gmail.com>
2019-10-16 00:44:02 +0800

    add code

---
+++ studyIterator.py
@@ -0,0 +1,14 @@
+# coding=utf-8
+print([x*x for x in range(1, 11)])
+
+L1 = ['Hello', 'World', 18, 'Apple', None]
+
+L2 = [L2.lower() for L2 in L1 if isinstance(L2, str)]
+
+print(L2)
+if L2 == ['hello', 'world', 'apple']:
+    print('测试通过!')
+else:
+    print('测试失败!')
+
+
